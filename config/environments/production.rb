1472fb1a2c63aaa2d6d8877608853810b27841bb
Josh <joshsimon85@yahoo.com>
2019-08-03 18:54:57 -0600

    Config mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,12 +18,13 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options =  { host: 'thawing-thicket-54932.herokuapp.com' }
+
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'thawing-thicket-54932.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
