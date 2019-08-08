67e5eabbff41bdd41adcca3d41f436fbdc854bef
Josh <joshsimon85@yahoo.com>
2019-08-08 16:27:17 -0600

    carrier-wave config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV[:aws_access_key_id],
-      :aws_secret_access_key  => ENV[:aws_secret_access_key],
+      :aws_access_key_id      => ENV['aws_access_key_id'],
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],
     }
     config.storage = :fog
     config.fog_provider = 'fog/aws'
