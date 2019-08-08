4d2ac8fd7ae7c22c2a960386ea1fc8b7b4100a47
Josh <joshsimon85@yahoo.com>
2019-08-08 17:09:59 -0600

    ...

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['aws_access_key_id'],
-      :aws_secret_access_key  => ENV['aws_secret_access_key'],
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET_ACCESS_KEY'],
     }
     config.storage = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_directory  = 'myflix-app-videos'
   else
     config.storage = :file
