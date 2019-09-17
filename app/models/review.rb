b252fcc3a9bd77526091fa8fdb45186f535f1005
Josh <joshsimon85@yahoo.com>
2019-09-17 16:47:05 -0600

    add rating search feature

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   default_scope { order(created_at: :desc) }
 
-  belongs_to :video
+  belongs_to :video, touch: true
   belongs_to :user
 
   validates_presence_of :body, :rating
--- app/models/video.rb
+++ app/models/video.rb
@@ -28,20 +28,46 @@ class Video < ActiveRecord::Base
     self.reviews.count
   end
 
-  def self.search(query)
+  def self.search(query, options={})
     search_definition = {
       query: {
-        multi_match: {
-          query: query,
-          fields: ['title', 'description'],
-          operator: 'and',
+        bool: {
+          must: {
+            multi_match: {
+              query: query,
+              fields: ['title^100', 'description^50'],
+              operator: 'and'
+            }
+          }
         }
       }
     }
+
+    if query.present? && options[:reviews].present?
+      search_definition[:query][:bool][:must][:multi_match][:fields] << 'reviews.body'
+    end
+
+    if options[:rating_from].present? || options[:rating_to].present?
+      search_definition[:query][:bool][:filter] = {
+        range: {
+          rating: {
+            gte: (options[:rating_from] if options[:rating_from].present?),
+            lte: (options[:rating_to] if options[:rating_to].present?)
+          }
+        }
+      }
+    end
+
     __elasticsearch__.search(search_definition)
   end
 
   def as_indexed_json(options={})
-    as_json(only: [:title, :description])
+    as_json(
+      methods: [:rating],
+      only: [:title, :description],
+      include: {
+        reviews: { only: [:body] }
+      }
+    )
   end
 end
